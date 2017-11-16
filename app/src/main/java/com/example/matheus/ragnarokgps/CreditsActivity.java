6ef832aff76e0242c771154a6ca309dca8172ed1
Matheus Ribeiro Garcia <matheus_rock14@hotmail.com>
2017-11-16 13:23:00 -0300

    Little Changes

--- app/src/main/java/com/example/matheus/ragnarokgps/CreditsActivity.java
+++ app/src/main/java/com/example/matheus/ragnarokgps/CreditsActivity.java
@@ -9,5 +9,11 @@ public class CreditsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_credits);
+
+
+
     }
+
+
+
 }
